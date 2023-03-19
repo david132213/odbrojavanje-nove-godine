782c5d88ca97d327bdd9330cd393d438bc142321
david132213 <127669496+david132213@users.noreply.github.com>
2023-03-19 15:18:43 +0100

    Add files via upload

---
+++ odbrojavanje do nove godine/assets/js/main.js
@@ -0,0 +1,42 @@
+days= document.getElementById('days');
+
+hours= document.getElementById('hours');
+
+minutes= document.getElementById('minutes');
+
+seconds= document.getElementById('seconds');
+
+countdown = document.getElementById('countdown');
+
+year=document.getElementById('year');
+
+loading=document.getElementById('loading');
+
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = `flex`;
+})
+
+currentYear = new Date ().getFullYear();
+
+setInterval(updateCountdown,1000)
+year.innerText = currentYear + 1;
+
+newYearTime= new Date (`1.1.${currentYear +1} 00:00:00`)
+function updateCountdown(){
+    currentTime =new Date ()
+
+    diff = newYearTime -currentTime;
+    d =Math.floor(diff/1000/60/60/24)
+    h =Math.floor(diff/1000/60/60)%24
+    m =Math.floor(diff/1000/60)%60
+    s =Math.floor(diff/1000)%60
+
+    days.innerText = d;
+    hours.innerText = h< 10 ? `0`+h:h;
+    minutes.innerText=m< 10 ? `0`+m:m;
+    seconds.innerText=s< 10 ? `0`+s:s;
+
+    
+
+}
